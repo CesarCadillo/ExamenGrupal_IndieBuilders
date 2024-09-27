8079c916fb0f705bf3af6662b4dc13d975eb9646
Belen Castrillon Peche <brooksamy137@gmail.com>
2024-09-27 00:04:32 -0500

    Tipos de estructuras

---
+++ ExGrupal_IndieBuilders/ExGrupal_IndieBuilders/DefenseStructure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExGrupal_IndieBuilders
+{
+    public class DefenseStructure:Structure,IAttacker
+    {
+        private int damage;
+
+        public DefenseStructure(string name, int price, int health, int damage, bool isPlayerTeam)
+        {
+            this.name = name;
+            this.price = price;
+            this.health = health;
+            this.damage = damage;
+            this.isPlayerTeam = isPlayerTeam;
+        }
+
+        public int GetDamage() => damage;
+
+        public void Attack(IEntity target)
+        {
+            target.TakeDamage(damage);
+        }
+    }
+}
